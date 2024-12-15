7de933c546440cf2e7c27844c976108e782d1d93
harisaltafb105 <haris_memon1@yahoo.com>
2024-12-15 22:00:05 +0500

    Second Commit

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-black w-full h-12 text-white text-lg flex items-center justify-center'>
-        <h2> @ Made by Haris Altaf </h2>
+    <div className='bg-slate-700 w-full h-14 text-white text-lg flex items-center justify-center'>
+        <h2> @ A Responsive Webpage Made by Haris Altaf ... </h2>
     </div>
   )
 }
