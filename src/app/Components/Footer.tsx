d62d00c60e4b369ffcd6f5dfa0f38c6038283548
harisaltafb105 <haris_memon1@yahoo.com>
2024-12-15 23:34:16 +0500

    Errors resolve

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-slate-700 w-full h-14 text-white text-lg flex items-center justify-center'>
+    <div className='bg-blue-700 w-full h-14 text-white text-lg flex items-center justify-center'>
         <h2> @ A Responsive Webpage Made by Haris Altaf ... </h2>
     </div>
   )
